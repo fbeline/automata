06df919d9f1ecf5a678641cca62e418db91ee95e
Felipe Beline <felipe@fbeline.com>
2024-05-25 19:40:24 -0300

    refactor config file

--- src/config.c
+++ src/config.c
@@ -6,6 +6,14 @@
 #define STARTUP_REGISTRY "Software\\Microsoft\\Windows\\CurrentVersion\\Run"
 #define APPLICATION_NAME "Automata"
 
+LONG OpenRegistry(REGSAM samDesired, HKEY *hKey, const char* subKey) {
+  LONG result = RegOpenKeyEx(HKEY_CURRENT_USER, subKey, 0, samDesired, hKey);
+  if (result != ERROR_SUCCESS) {
+    Log(LOG_ERROR, "Fail to open registry %s. Error code: %ld", subKey, result);
+  }
+  return result;
+}
+
 DWORD GetExePath(char *path) {
   HMODULE hModule = GetModuleHandle(NULL);
 
@@ -18,21 +26,17 @@ DWORD GetExePath(char *path) {
 }
 
 int GetRegistryValue(const char* subKey, const char* entry, char* value) {
-  DWORD bufferSize = MAX_PATH;
   HKEY hKey;
-  LONG result;
-
-  result = RegOpenKeyEx(HKEY_CURRENT_USER, subKey, 0, KEY_READ, &hKey);
-  if (result != ERROR_SUCCESS) {
-    if (result != 2) // do not log error if registry just does not exists
-      Log(LOG_ERROR, "Fail to open registry %s. Error code: %ld", subKey, result);
+  if (OpenRegistry(KEY_READ, &hKey, subKey) != ERROR_SUCCESS)
     return 1;
-  }
 
-  result = RegQueryValueEx(hKey, entry, NULL, NULL, (LPBYTE)value, &bufferSize);
+  DWORD bufferSize = MAX_PATH;
+  LONG result = RegQueryValueEx(hKey, entry, NULL, NULL, (LPBYTE)value, &bufferSize);
   if (result != ERROR_SUCCESS) {
+    if (result != 2) // do not log when key is missing
+      Log(LOG_ERROR, "Fail to read registry \"%s\". Error code: %ld", entry, result);
+
     RegCloseKey(hKey);
-    Log(LOG_ERROR, "Fail to read registry \"%s\". Error code: %ld", entry, result);
     return 2;
   }
   RegCloseKey(hKey);
@@ -42,20 +46,17 @@ int GetRegistryValue(const char* subKey, const char* entry, char* value) {
 
 int DeleteRegistryKey(const char* subKey, const char* entry) {
   HKEY hKey;
-  LONG result;
-
-  result = RegOpenKeyEx(HKEY_CURRENT_USER, subKey, 0, KEY_SET_VALUE, &hKey);
-  if (result != ERROR_SUCCESS) {
-    Log(LOG_ERROR, "Fail to open registry %s. Error code: %ld", subKey, result);
+  if (OpenRegistry(KEY_SET_VALUE, &hKey, subKey) != ERROR_SUCCESS)
     return 1;
-  }
 
-  result = RegDeleteValue(hKey, entry);
+  LONG result = RegDeleteValue(hKey, entry);
   if (result != ERROR_SUCCESS) {
-    Log(LOG_ERROR, "Fail to delete registry \"%s\". Error code: %ld", entry, result);
+    Log(LOG_ERROR, "Fail to delete registry \"%s\\%s\". Error code: %ld", subKey, entry, result);
     RegCloseKey(hKey);
     return 2;
   }
+
+  Log(LOG_INFO, "Successfully delete key \"%s\\%s\"", subKey, entry);
 
   RegCloseKey(hKey);
   return 0;
@@ -71,26 +72,24 @@ bool IsStartingWithSystem(void) {
 }
 
 int StartWithWindows(void) {
-  HKEY hKey;
   char czExePath[MAX_PATH];
   DWORD pathSize = GetExePath(czExePath);
-
   if (pathSize == 0) return 1;
 
-  if (RegOpenKeyEx(HKEY_CURRENT_USER, STARTUP_REGISTRY, 0, KEY_SET_VALUE, &hKey) == ERROR_SUCCESS) {
-    if (RegSetValueEx(hKey, APPLICATION_NAME, 0, REG_SZ, (BYTE*)czExePath, strlen(czExePath) + 1) == ERROR_SUCCESS) {
-      RegCloseKey(hKey);
-      Log(LOG_INFO, "Successfully added automata to windows startup: (%s)", czExePath);
-    } else {
-      RegCloseKey(hKey);
-      Log(LOG_ERROR, "Error adding automata to windows startup");
-      return 2;
-    }
-  } else {
-    Log(LOG_ERROR, "Error opening registry key");
+  HKEY hKey;
+  if (OpenRegistry(KEY_SET_VALUE, &hKey, STARTUP_REGISTRY) != ERROR_SUCCESS)
     return 1;
+
+  LONG result = RegSetValueEx(hKey, APPLICATION_NAME, 0, REG_SZ, (BYTE*)czExePath, strlen(czExePath) + 1);
+  if (result == ERROR_SUCCESS) {
+    Log(LOG_INFO, "Successfully added automata to windows startup: (%s)", czExePath);
+  } else {
+    RegCloseKey(hKey);
+    Log(LOG_ERROR, "Error adding automata to windows startup. Error code: %ld", result);
+    return 2;
   }
 
+  RegCloseKey(hKey);
   return 0;
 }
 
